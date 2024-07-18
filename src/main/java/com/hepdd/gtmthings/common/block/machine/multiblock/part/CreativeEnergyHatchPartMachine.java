17b385ee22e9fe625923bf7bd3cc56594942bacb
liansishen <liansishen1992@gmail.com>
2024-07-19 00:20:21 +0800

    修改创造模式能源仓、激光靶仓为可调节模式

--- src/main/java/com/hepdd/gtmthings/common/block/machine/multiblock/part/CreativeEnergyHatchPartMachine.java
+++ src/main/java/com/hepdd/gtmthings/common/block/machine/multiblock/part/CreativeEnergyHatchPartMachine.java
@@ -2,21 +2,43 @@ package com.hepdd.gtmthings.common.block.machine.multiblock.part;
 
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
+import com.gregtechceu.gtceu.api.gui.GuiTextures;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.TickableSubscription;
-import com.gregtechceu.gtceu.api.machine.feature.IExplosionMachine;
+import com.gregtechceu.gtceu.api.machine.feature.IDataInfoProvider;
 import com.gregtechceu.gtceu.api.machine.multiblock.part.TieredIOPartMachine;
 import com.gregtechceu.gtceu.api.machine.trait.NotifiableEnergyContainer;
+import com.gregtechceu.gtceu.common.item.PortableScannerBehavior;
+import com.gregtechceu.gtceu.utils.GTUtil;
+import com.lowdragmc.lowdraglib.gui.editor.ColorPattern;
+import com.lowdragmc.lowdraglib.gui.modular.ModularUI;
+import com.lowdragmc.lowdraglib.gui.texture.GuiTextureGroup;
+import com.lowdragmc.lowdraglib.gui.texture.ResourceBorderTexture;
+import com.lowdragmc.lowdraglib.gui.texture.TextTexture;
+import com.lowdragmc.lowdraglib.gui.widget.ButtonWidget;
+import com.lowdragmc.lowdraglib.gui.widget.LabelWidget;
+import com.lowdragmc.lowdraglib.gui.widget.SelectorWidget;
+import com.lowdragmc.lowdraglib.gui.widget.TextFieldWidget;
 import com.lowdragmc.lowdraglib.syncdata.ISubscription;
 import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
 import lombok.Getter;
-import net.minecraft.world.InteractionHand;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.BlockHitResult;
+import org.apache.commons.lang3.ArrayUtils;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class CreativeEnergyHatchPartMachine extends TieredIOPartMachine implements IExplosionMachine {
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class CreativeEnergyHatchPartMachine extends TieredIOPartMachine implements IDataInfoProvider {
 
 
     protected static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(CreativeEnergyHatchPartMachine.class, TieredIOPartMachine.MANAGED_FIELD_HOLDER);
@@ -26,14 +48,18 @@ public class CreativeEnergyHatchPartMachine extends TieredIOPartMachine implemen
     protected TickableSubscription explosionSubs;
     @Nullable
     protected ISubscription energyListener;
-    @Getter
-    protected int amperage;
     private Long maxEnergy;
+    @Persisted
+    private long voltage = 0;
+    @Persisted
+    @Getter
+    private int amps = 1;
+    @Persisted
+    private int setTier = 0;
 
-    public CreativeEnergyHatchPartMachine(IMachineBlockEntity holder, int tier, int amperage, Object... args) {
-        super(holder, tier, IO.IN);
-        this.amperage = amperage;
-        this.energyContainer = createEnergyContainer(args);
+    public CreativeEnergyHatchPartMachine(IMachineBlockEntity holder) {
+        super(holder, GTValues.MAX, IO.IN);
+        this.energyContainer = createEnergyContainer();
     }
 
     //////////////////////////////////////
@@ -44,16 +70,11 @@ public class CreativeEnergyHatchPartMachine extends TieredIOPartMachine implemen
         return MANAGED_FIELD_HOLDER;
     }
 
-    protected NotifiableEnergyContainer createEnergyContainer(Object... args) {
+    protected NotifiableEnergyContainer createEnergyContainer() {
         NotifiableEnergyContainer container;
-        this.maxEnergy = GTValues.V[tier] * 16L * amperage;
-        container = NotifiableEnergyContainer.receiverContainer(this, this.maxEnergy , GTValues.V[tier], amperage);
+        this.maxEnergy = GTValues.V[tier] * 16L * amps;
+        container = NotifiableEnergyContainer.receiverContainer(this, this.maxEnergy , GTValues.V[tier], amps);
         return container;
-    }
-
-    @Override
-    public boolean shouldOpenUI(Player player, InteractionHand hand, BlockHitResult hit) {
-        return false;
     }
 
     @Override
@@ -77,10 +98,51 @@ public class CreativeEnergyHatchPartMachine extends TieredIOPartMachine implemen
     }
 
     protected void addEnergy() {
+        if (energyContainer.getInputVoltage() != voltage || energyContainer.getInputAmperage() != amps) {
+            maxEnergy = voltage * 16L * amps;
+            energyContainer.resetBasicInfo(maxEnergy, voltage, amps,0,0);
+            energyContainer.setEnergyStored(0);
+        }
         if (energyContainer.getEnergyStored() < this.maxEnergy ) {
             energyContainer.setEnergyStored(this.maxEnergy );
         }
+    }
 
+
+
+    @Override
+    public ModularUI createUI(Player entityPlayer) {
+        return new ModularUI(176, 136, this, entityPlayer)
+                .background(GuiTextures.BACKGROUND)
+                .widget(new LabelWidget(7, 32, "gtceu.creative.energy.voltage"))
+                .widget(new TextFieldWidget(9, 47, 152, 16, () -> String.valueOf(voltage),
+                        value -> {
+                            voltage = Long.parseLong(value);
+                            setTier = GTUtil.getTierByVoltage(voltage);
+                        }).setNumbersOnly(8L, Long.MAX_VALUE))
+                .widget(new LabelWidget(7, 74, "gtceu.creative.energy.amperage"))
+                .widget(new ButtonWidget(7, 87, 20, 20,
+                        new GuiTextureGroup(ResourceBorderTexture.BUTTON_COMMON, new TextTexture("-")),
+                        cd -> amps = --amps == -1 ? 0 : amps))
+                .widget(new TextFieldWidget(31, 89, 114, 16, () -> String.valueOf(amps),
+                        value -> amps = Integer.parseInt(value)).setNumbersOnly(1, 67108864))
+                .widget(new ButtonWidget(149, 87, 20, 20,
+                        new GuiTextureGroup(ResourceBorderTexture.BUTTON_COMMON, new TextTexture("+")),
+                        cd -> {
+                            if (amps < Integer.MAX_VALUE) {
+                                amps++;
+                            }
+                        }))
+
+                .widget(new SelectorWidget(7, 7, 30, 20, Arrays.stream(GTValues.VNF).toList(), -1)
+                        .setOnChanged(tier -> {
+                            setTier = ArrayUtils.indexOf(GTValues.VNF, tier);
+                            voltage = GTValues.V[setTier];
+                        })
+                        .setSupplier(() -> GTValues.VNF[setTier])
+                        .setButtonBackground(ResourceBorderTexture.BUTTON_COMMON)
+                        .setBackground(ColorPattern.BLACK.rectTexture())
+                        .setValue(GTValues.VNF[setTier]));
     }
 
     //////////////////////////////////////
@@ -93,5 +155,15 @@ public class CreativeEnergyHatchPartMachine extends TieredIOPartMachine implemen
             return GTValues.VC[getTier()];
         }
         return super.tintColor(index);
+    }
+
+    @NotNull
+    @Override
+    public List<Component> getDataInfo(PortableScannerBehavior.DisplayMode mode) {
+        if (mode == PortableScannerBehavior.DisplayMode.SHOW_ALL || mode == PortableScannerBehavior.DisplayMode.SHOW_ELECTRICAL_INFO) {
+            return Collections.singletonList(Component.literal(
+                    String.format("%d/%d EU", energyContainer.getEnergyStored(), energyContainer.getEnergyCapacity())));
+        }
+        return new ArrayList<>();
     }
 }
--- src/main/java/com/hepdd/gtmthings/common/block/machine/multiblock/part/CreativeLaserHatchPartMachine.java
+++ src/main/java/com/hepdd/gtmthings/common/block/machine/multiblock/part/CreativeLaserHatchPartMachine.java
@@ -2,27 +2,40 @@ package com.hepdd.gtmthings.common.block.machine.multiblock.part;
 
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
+import com.gregtechceu.gtceu.api.gui.GuiTextures;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.TickableSubscription;
 import com.gregtechceu.gtceu.api.machine.feature.IDataInfoProvider;
 import com.gregtechceu.gtceu.api.machine.multiblock.part.TieredIOPartMachine;
 import com.gregtechceu.gtceu.api.machine.trait.NotifiableLaserContainer;
 import com.gregtechceu.gtceu.common.item.PortableScannerBehavior;
+import com.gregtechceu.gtceu.utils.GTUtil;
+import com.lowdragmc.lowdraglib.gui.editor.ColorPattern;
+import com.lowdragmc.lowdraglib.gui.modular.ModularUI;
+import com.lowdragmc.lowdraglib.gui.texture.GuiTextureGroup;
+import com.lowdragmc.lowdraglib.gui.texture.ResourceBorderTexture;
+import com.lowdragmc.lowdraglib.gui.texture.TextTexture;
+import com.lowdragmc.lowdraglib.gui.widget.ButtonWidget;
+import com.lowdragmc.lowdraglib.gui.widget.LabelWidget;
+import com.lowdragmc.lowdraglib.gui.widget.SelectorWidget;
+import com.lowdragmc.lowdraglib.gui.widget.TextFieldWidget;
 import com.lowdragmc.lowdraglib.syncdata.ISubscription;
 import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
 import lombok.Getter;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.BlockHitResult;
+import org.apache.commons.lang3.ArrayUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import static net.minecraft.ChatFormatting.*;
 
 @ParametersAreNonnullByDefault
 public class CreativeLaserHatchPartMachine extends TieredIOPartMachine implements IDataInfoProvider {
@@ -33,15 +46,32 @@ public class CreativeLaserHatchPartMachine extends TieredIOPartMachine implement
     @Nullable
     protected ISubscription LaserListener;
     protected TickableSubscription explosionSubs;
-    @Getter
-    protected int amperage;
     private Long maxEnergy;
+    @Persisted
+    private long voltage = 0;
+    @Persisted
+    @Getter
+    private int amps = 256;
+    @Persisted
+    private int setTier = GTValues.IV;
 
-    public CreativeLaserHatchPartMachine(IMachineBlockEntity holder, int tier, int amperage) {
-        super(holder, tier, IO.IN);
-        this.maxEnergy = GTValues.V[tier] * 64L * amperage;
-        this.buffer = NotifiableLaserContainer.receiverContainer(this,this.maxEnergy , GTValues.V[tier], amperage);
-        this.amperage = amperage;
+    public static final String[] VNF = new String[] {
+            BLUE + "IV",
+            LIGHT_PURPLE + "LuV",
+            RED + "ZPM",
+            DARK_AQUA + "UV",
+            DARK_RED + "UHV",
+            GREEN + "UEV",
+            DARK_GREEN + "UIV",
+            YELLOW + "UXV",
+            BLUE.toString() + BOLD + "OpV",
+            RED.toString() + BOLD + "MAX" };
+
+    public CreativeLaserHatchPartMachine(IMachineBlockEntity holder) {
+        super(holder, GTValues.MAX, IO.IN);
+        this.voltage = GTValues.V[setTier];
+        this.maxEnergy = voltage * 64L * amps;
+        this.buffer = NotifiableLaserContainer.receiverContainer(this,this.maxEnergy , voltage, amps);
     }
 
     @Override
@@ -66,15 +96,14 @@ public class CreativeLaserHatchPartMachine extends TieredIOPartMachine implement
     }
 
     protected void addEng() {
+        if (buffer.getInputVoltage() != voltage || buffer.getInputAmperage() != amps) {
+            maxEnergy = voltage * 64L * amps;
+            buffer.resetBasicInfo(maxEnergy, voltage, amps,0,0);
+            buffer.setEnergyStored(0);
+        }
         if (buffer.getEnergyStored() < this.maxEnergy) {
             buffer.setEnergyStored(this.maxEnergy);
         }
-
-    }
-
-    @Override
-    public boolean shouldOpenUI(Player player, InteractionHand hand, BlockHitResult hit) {
-        return false;
     }
 
     @Override
@@ -86,6 +115,41 @@ public class CreativeLaserHatchPartMachine extends TieredIOPartMachine implement
     @NotNull
     public ManagedFieldHolder getFieldHolder() {
         return MANAGED_FIELD_HOLDER;
+    }
+
+    @Override
+    public ModularUI createUI(Player entityPlayer) {
+        return new ModularUI(176, 136, this, entityPlayer)
+                .background(GuiTextures.BACKGROUND)
+                .widget(new LabelWidget(7, 32, "gtceu.creative.energy.voltage"))
+                .widget(new TextFieldWidget(9, 47, 152, 16, () -> String.valueOf(voltage),
+                        value -> {
+                            voltage = Long.parseLong(value);
+                            setTier = GTUtil.getTierByVoltage(voltage);
+                        }).setNumbersOnly(8192L, Long.MAX_VALUE))
+                .widget(new LabelWidget(7, 74, "gtceu.creative.energy.amperage"))
+                .widget(new ButtonWidget(7, 87, 20, 20,
+                        new GuiTextureGroup(ResourceBorderTexture.BUTTON_COMMON, new TextTexture("-")),
+                        cd -> amps = --amps == -1 ? 0 : amps))
+                .widget(new TextFieldWidget(31, 89, 114, 16, () -> String.valueOf(amps),
+                        value -> amps = Integer.parseInt(value)).setNumbersOnly(256, 67108864))
+                .widget(new ButtonWidget(149, 87, 20, 20,
+                        new GuiTextureGroup(ResourceBorderTexture.BUTTON_COMMON, new TextTexture("+")),
+                        cd -> {
+                            if (amps < Integer.MAX_VALUE) {
+                                amps++;
+                            }
+                        }))
+
+                .widget(new SelectorWidget(7, 7, 30, 20, Arrays.stream(VNF).toList(), -1)
+                        .setOnChanged(tier -> {
+                            setTier = ArrayUtils.indexOf(VNF, tier) + 5;
+                            voltage = GTValues.V[setTier];
+                        })
+                        .setSupplier(() -> VNF[setTier - 5])
+                        .setButtonBackground(ResourceBorderTexture.BUTTON_COMMON)
+                        .setBackground(ColorPattern.BLACK.rectTexture())
+                        .setValue(VNF[setTier - 5]));
     }
 
     @NotNull
