06ddc4546d79e27219bdb3c73102f8adf7737990
aaa <1040179812@qq.com>
2025-04-02 19:25:02 +0800

    spotless

--- src/main/java/com/hepdd/gtmthings/common/item/VirtualItemProviderBehavior.java
+++ src/main/java/com/hepdd/gtmthings/common/item/VirtualItemProviderBehavior.java
@@ -24,7 +24,6 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.items.IItemHandlerModifiable;
