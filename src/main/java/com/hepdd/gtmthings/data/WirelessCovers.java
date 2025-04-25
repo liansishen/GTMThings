bb5a556d41dde04a1505f96411c2acca5a109f45
liansishen <liansishen1992@gmail.com>
2025-04-26 01:02:48 +0800

    Add creative energy cover.

--- src/main/java/com/hepdd/gtmthings/data/WirelessCovers.java
+++ src/main/java/com/hepdd/gtmthings/data/WirelessCovers.java
@@ -9,6 +9,7 @@ import com.gregtechceu.gtceu.client.renderer.cover.SimpleCoverRenderer;
 
 import com.hepdd.gtmthings.GTMThings;
 import com.hepdd.gtmthings.common.cover.AdvancedWirelessTransferCover;
+import com.hepdd.gtmthings.common.cover.CreativeEnergyCover;
 import com.hepdd.gtmthings.common.cover.WirelessEnergyReceiveCover;
 import com.hepdd.gtmthings.common.cover.WirelessTransferCover;
 import it.unimi.dsi.fastutil.ints.Int2ObjectFunction;
@@ -43,6 +44,9 @@ public class WirelessCovers {
 
     public final static CoverDefinition ADVANCED_WIRELESS_FLUID_TRANSFER = register("advanced_wireless_fluid_transfer",
             (holder, coverable, side) -> new AdvancedWirelessTransferCover(holder, coverable, side, WirelessTransferCover.TRANSFER_FLUID), new SimpleCoverRenderer(GTMThings.id("block/cover/overlay_wireless_fluid_transfer")));
+
+    public final static CoverDefinition CREATIVE_ENERGY = register("creative_energy",
+            CreativeEnergyCover::new, new SimpleCoverRenderer(GTMThings.id("block/cover/overlay_creative_energy")));
 
     public static CoverDefinition register(String id, CoverDefinition.CoverBehaviourProvider behaviorCreator,
                                            ICoverRenderer coverRenderer) {
