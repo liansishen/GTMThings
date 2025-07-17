a7bef50b36f7cb031c73dc42ddc342aeb0debf99
liansishen <liansishen1992@gmail.com>
2025-07-17 22:21:49 +0800

    GTO port

--- src/main/java/com/hepdd/gtmthings/utils/FormatUtil.java
+++ src/main/java/com/hepdd/gtmthings/utils/FormatUtil.java
@@ -1,16 +1,28 @@
 package com.hepdd.gtmthings.utils;
 
-import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.utils.GTUtil;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.ComponentCollector;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.util.FormattedCharSequence;
+
+import com.google.common.collect.Lists;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.gregtechceu.gtceu.utils.FormattingUtil.DECIMAL_FORMAT_SIC_2F;
 import static com.gregtechceu.gtceu.utils.FormattingUtil.formatNumberReadable;
@@ -37,22 +49,15 @@ public class FormatUtil {
         return number.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0 ? DECIMAL_FORMAT_SIC_2F.format(number) : formatNumberReadable(number.longValue());
     }
 
-    public static MutableComponent formatWithConstantWidth(String labelKey, int width, Component body, Component... appends) {
+    public static MutableComponent formatWithConstantWidth(String labelKey, Component body, Component... appends) {
         var a = new Component[appends.length + 1];
         a[0] = body;
         int i = 0;
         for (var c : appends) {
             a[++i] = c;
         }
-        var tmp = Component.translatable(labelKey, (Object[]) a);
-        var baseLength = getComponentLength(tmp);
-        var spaceLength = width - baseLength;
-        if (spaceLength <= 0) return tmp;
-        // var spacerCount = (spaceLength / 2) - 4;
-        var spacerCount = 0; // 暂时移除对齐功能
-        var spacer = spacerCount > 0 ? (".".repeat((spaceLength / 2) - 4) + " ") : "";
+        var spacer = ".";
         var spacerComponent = Component.literal(spacer);
-        // return tmp;
         a[0] = spacerComponent.append(body);
         return Component.translatable(labelKey, (Object[]) a);
     }
@@ -65,11 +70,42 @@ public class FormatUtil {
         return avgEnergy.abs().divide(BigDecimal.valueOf(GTValues.VEX[GTUtil.getFloorTierByVoltage(avgEnergy.abs().longValue())]), 1, RoundingMode.FLOOR);
     }
 
-    private static int getComponentLength(Component component) {
-        if (GTCEu.isClientSide()) {
-            return Minecraft.getInstance().font.width(component.getString());
-        } else {
-            return component.getString().length() / 2;
+    public static String getSpacer(Font font, String splitChar, int spaceLength) {
+        int spacerCount = spaceLength / font.width(splitChar);
+        while (font.width(splitChar.repeat(spacerCount) + " ") <= spaceLength) {
+            spacerCount++;
         }
+        return splitChar.repeat(spacerCount - 2) + " ";
+    }
+
+    public static List<FormattedCharSequence> formatJustifyComponent(FormattedText component, int maxWidth, Font font, String splitChar) {
+        ComponentCollector componentcollector = new ComponentCollector();
+        AtomicInteger before = new AtomicInteger();
+        AtomicInteger after = new AtomicInteger();
+        AtomicBoolean hasSplit = new AtomicBoolean(false);
+        component.visit((style, text) -> {
+            if (text.equals(splitChar)) {
+                hasSplit.set(true);
+            } else {
+                int width = font.width(text);
+                (hasSplit.get() ? after : before).getAndAdd(width);
+            }
+            return Optional.empty();
+        }, Style.EMPTY);
+        component.visit((style, text) -> {
+            String content = text.equals(splitChar) ? getSpacer(font, splitChar, maxWidth - before.get() - after.get()) : text;
+            componentcollector.append(FormattedText.of(stripColor(content), style));
+            return Optional.empty();
+        }, Style.EMPTY);
+        List<FormattedCharSequence> list = Lists.newArrayList();
+        font.getSplitter().splitLines(componentcollector.getResultOrEmpty(), maxWidth, Style.EMPTY, (text, p_94004_) -> {
+            FormattedCharSequence formattedcharsequence = Language.getInstance().getVisualOrder(text);
+            list.add(p_94004_ ? FormattedCharSequence.composite(FormattedCharSequence.codepoint(32, Style.EMPTY), formattedcharsequence) : formattedcharsequence);
+        });
+        return (list.isEmpty() ? Lists.newArrayList(FormattedCharSequence.EMPTY) : list);
+    }
+
+    private static String stripColor(String text) {
+        return Minecraft.getInstance().options.chatColors().get() ? text : ChatFormatting.stripFormatting(text);
     }
 }
