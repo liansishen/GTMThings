f47efce1c35cf0d89f4819d9f9d090f220a518f1
remakefactory <remakefactory5@gmail.com>
2025-10-10 00:44:04 +0800

    创造能源覆盖版 电流调整 (#108)

--- src/main/java/com/hepdd/gtmthings/common/cover/CreativeEnergyCover.java
+++ src/main/java/com/hepdd/gtmthings/common/cover/CreativeEnergyCover.java
@@ -11,8 +11,13 @@ import com.gregtechceu.gtceu.api.machine.TieredEnergyMachine;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.BlockHitResult;
 
 import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
@@ -31,6 +36,8 @@ public class CreativeEnergyCover extends CoverBehavior {
             CoverBehavior.MANAGED_FIELD_HOLDER);
 
     private TickableSubscription subscription;
+
+    private static final int MAX_AMPERAGE = 16;
 
     @Persisted
     private long energyPerTick;
@@ -91,6 +98,24 @@ public class CreativeEnergyCover extends CoverBehavior {
             this.machineMaxEnergy = GTValues.VEX[tieredEnergyMachine.getTier()] << 6;
         }
         updateCoverSub();
+    }
+
+    @Override
+    public InteractionResult onScrewdriverClick(Player player, InteractionHand hand, BlockHitResult hitResult) {
+        if (!coverHolder.getLevel().isClientSide()) {
+            if (this.amperage >= MAX_AMPERAGE) {
+                this.amperage = 1;
+            } else {
+                this.amperage *= 2;
+            }
+            updatePower(player);
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    private void updatePower(Player player) {
+        this.energyPerTick = GTValues.VEX[this.tier] * this.amperage;
+        player.displayClientMessage(Component.literal(this.amperage + "A"), false);
     }
 
     private void updateCoverSub() {
